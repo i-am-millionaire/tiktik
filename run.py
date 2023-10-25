6bfeb68ebdb22e83dd4e143846cd2d4843383cc2
vendannotes <111574533+i-am-millionaire@users.noreply.github.com>
2023-10-25 18:48:00 +0530

    Update run.py

--- run.py
+++ run.py
@@ -1,55 +1,29 @@
-from hashlib import new
-from random import getrandbits
-from time import time
-from threading import Thread
-from multiprocessing import Pool
+1. import hashlib
+2. import requests
+3. import time
+4. import random
+5. import threading
 
-import pycuda.autoinit
-import pycuda.driver as drv
-from pycuda.compiler import SourceModule
-
+# Define your Monero wallet address
 wallet_address = "45NMFRwkrCKVTDPid126qSdxyAD6XxyvcJ2EkvzPveK9ajsotv7gp2cdCr4aCv8e2G7jLiyfvZDdXSZ4LGAqMj8ZL49buet"
-
-# This function should be implemented by you.
-def cuda_hash_function(data):
-    pass
-
-# Load CUDA source code as a string.
-cuda_code = """
-    __global__ void cuda_hash_function(const char* data, char* result) {
-        // Implement your hash function here.
-    }
-"""
-
-# Create a CUDA context and prepare the data.
-mod = SourceModule(cuda_code)
-hash_function = mod.get_function("cuda_hash_function")
-data = wallet_address + str(getrandbits(64))
 
 # Create a function for mining
 def mine():
     while True:
-        # Prepare data and run the kernel on the GPU.
-        drv.memcpy_htod(gpu_data, data.encode())
-        drv.memcpy_htod(gpu_result, b"0000000000000000")
-        hash_function(gpu_data, gpu_result, block=(1,1,1), grid=(1,1))
-        drv.memcpy_dtoh(result, gpu_result)
+        nonce = random.getrandbits(64)
+        data = wallet_address + str(nonce)
+        hash_result = hashlib.new('ripemd160', data.encode()).hexdigest()
 
-        # Check if the hash is valid.
-        if result.decode()[:5] == "00000":
-            print(f"Hash found: {result.decode()}")
+        if hash_result[:5] == "00000":  # Adjust the difficulty as needed
+            print(f"Hash found: {hash_result}")
+            # You can send this result to your mining pool or wallet
 
 # Set up multiple threads for parallel mining
-num_threads = 4
+num_threads = 50000  # You can adjust the number of threads
 threads = []
 
-# Create a GPU context and allocate memory.
-ctx = drv.Device(0).make_context()
-gpu_data = drv.mem_alloc(len(data.encode()))
-gpu_result = drv.mem_alloc(64)
-
 for _ in range(num_threads):
-    t = Thread(target=mine)
+    t = threading.Thread(target=mine)
     threads.append(t)
 
 for t in threads:
@@ -57,6 +31,3 @@ for t in threads:
 
 for t in threads:
     t.join()
-
-# Clean up the GPU context.
-ctx.pop()
